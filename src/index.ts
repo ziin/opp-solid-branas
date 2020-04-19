bc3ea97ecaf3658104114ae0a7f75bb9e5a6bd4f
Alisson L. Schwanz <alissonludtkeschwanz@gmail.com>
2020-04-19 15:45:16 -0400

    update procedural

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,3 @@
-import AbstractShareButton from "./AbstractShareButton";
-import DOMEventHandler from "./DOMEventHandler";
-import ShareButtonFacebook from "./ShareButtonFacebook";
-import ShareButtonLinkedin from "./ShareButtonLinkedin";
-import ShareButtonPrint from "./ShareButtonPrint";
-import ShareButtonTwitter from "./ShareButtonTwitter";
+import "./procedural";
 
-const eventHandler = new DOMEventHandler();
-// const eventHandler = new LogEventHandler();
 
-const facebook: AbstractShareButton = new ShareButtonFacebook(
-  eventHandler,
-  "btn--facebook",
-  "https://www.google.com"
-);
-facebook.bind();
-
-const twitter: AbstractShareButton = new ShareButtonTwitter(
-  eventHandler,
-  "btn--twitter",
-  "https://www.google.com"
-);
-twitter.bind();
-
-const linkedin: AbstractShareButton = new ShareButtonLinkedin(
-  eventHandler,
-  "btn--linkedin",
-  "https://www.google.com"
-);
-linkedin.bind();
-
-const print: AbstractShareButton = new ShareButtonPrint(
-  eventHandler,
-  "btn--print"
-);
-print.bind();
---
+++ src/procedural/index.ts
@@ -0,0 +1,63 @@
+function addListenerToButtons(
+  event: string,
+  classNames: string[],
+  cb: (event: Event, button: HTMLButtonElement) => void
+) {
+  for (const className of classNames) {
+    const buttons = document.getElementsByClassName(className);
+    for (const button of buttons) {
+      if (button instanceof HTMLButtonElement) {
+        button.addEventListener(event, (event) => cb(event, button));
+      }
+    }
+  }
+}
+
+// addListenerToButtons(
+//   "click",
+//   ["btn--facebook", "btn--twitter", "btn--linkedin", "btn--print"],
+//   (e, button) => {
+//     const url = "https://www.google.com";
+//     if (button.classList.contains("btn--facebook")) {
+//       window.open(`https://facebook.com/share.php?u=${url}`);
+//     } else if (button.classList.contains("btn--twitter")) {
+//       window.open(`https://twitter.com/share?url=${url}`);
+//     } else if (button.classList.contains("btn--linkedin")) {
+//       window.open(`https://linkedin.com/shareArticle?url=${url}`);
+//     } else if (button.classList.contains("btn--print")) {
+//       window.print();
+//     }
+//   }
+// );
+
+// Alternative way
+function bindEventListenerToClassNames(
+  event: string,
+  classNames: string | string[],
+  cb: () => void
+) {
+  for (const element of document.getElementsByClassName(
+    typeof classNames === "string" ? classNames : classNames.join(" ")
+  )) {
+    element.addEventListener(event, cb);
+  }
+}
+
+bindEventListenerToClassNames("click", ["btn--twitter"], () =>
+  window.open("https://twitter.com/share?url=https://www.google.com")
+);
+
+bindEventListenerToClassNames("click", "btn--print", () => window.print());
+
+// Alternative
+function bindOnClickOpenUrlByClassNames(classNames: string[], url: string) {
+  for (const element of document.getElementsByClassName(classNames.join(" "))) {
+    element.addEventListener("click", () => window.open(url));
+  }
+}
+
+function bindOnClickOpenPrint(classNames: string[]) {
+  for (const element of document.getElementsByClassName(classNames.join(" "))) {
+    element.addEventListener("click", () => window.print());
+  }
+}
